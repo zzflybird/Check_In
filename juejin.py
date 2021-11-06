5a492e85218817f5d0f6556a01796a6b7cf7117d
mz <1069732181@qq.com>
2021-11-06 10:17:40 +0800

    添加retry()函数

--- juejin.py
+++ juejin.py
@@ -26,6 +26,8 @@ def Sliding_Captcha(driver):
     ActionChains(driver).release(on_element=verify_div).perform()
     time.sleep(10)
 
+
+@retry(stop_max_attempt_number=5)
 def juejin():
         flag = True
         while flag:
